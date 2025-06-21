d1bff319d43b8a191750cd8f527985d841351b00
heros20 <heros40@hotmail.fr>
2025-06-21 19:09:01 +0200

    favicon

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Les Comets d'Honfleur</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Site officiel de l'équipe de baseball Les Comets d'Honfleur" />
+    </>
+  )
+}
