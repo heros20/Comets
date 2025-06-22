9f1a2769cb88c2f93d3947d605f7fbb695bec2f0
heros20 <heros40@hotmail.fr>
2025-06-22 20:03:14 +0200

    modif refreshInterval stat 1.1

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -7,7 +7,7 @@ export default function Stats() {
   const { data: stats, isLoading } = useSWR(
     "/api/stats",
     fetcher,
-    { refreshInterval: 60000 // 60 000 ms = 1 minute } //
+    { refreshInterval: 60000 } // ← toutes les 1 minute,
   );
 
   if (isLoading || !stats) return (
