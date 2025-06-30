04b63d75da53a3f71f043f9573e26b19a7e7d0c6
heros20 <heros40@hotmail.fr>
2025-07-01 01:38:14 +0200

    upgrade SEO HEAD JSON-LD

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+// app/head.tsx
+
 export default function Head() {
   return (
     <>
@@ -8,13 +10,12 @@ export default function Head() {
       />
       <meta
         name="keywords"
-        content="baseball, comets honfleur, équipe baseball, baseball normandie, sport honfleur, entraînements baseball"
+        content="baseball, comets honfleur, équipe baseball, baseball normandie, sport honfleur, entraînements baseball, honfleur baseball"
       />
       <meta name="author" content="Les Comets d'Honfleur" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-
       <link rel="icon" href="/favicon.ico" />
-      <link rel="canonical" href="https://les-comets-honfleur.vercel.app/.vercel.app" />
+      <link rel="canonical" href="https://les-comets-honfleur.vercel.app/" />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
@@ -23,7 +24,7 @@ export default function Head() {
         property="og:description"
         content="Rejoignez Les Comets d'Honfleur, l'équipe de baseball de la région. Suivez nos actualités, matchs et entraînements."
       />
-      <meta property="og:url" content="https://les-comets-honfleur.vercel.app" />
+      <meta property="og:url" content="https://les-comets-honfleur.vercel.app/" />
       <meta property="og:image" content="https://les-comets-honfleur.vercel.app/images/baseballwallpaper.webp" />
 
       {/* Twitter */}
@@ -34,8 +35,11 @@ export default function Head() {
         content="Rejoignez Les Comets d'Honfleur, l'équipe de baseball de la région. Suivez nos actualités, matchs et entraînements."
       />
       <meta name="twitter:image" content="https://les-comets-honfleur.vercel.app/images/baseballwallpaper.webp" />
+
+      {/* Google Search Console */}
       <meta name="google-site-verification" content="gwi_1N79m9NaFbKmzhg8K9EOL6pv8kxcYFPCm_7YS58" />
-      {/* JSON-LD pour SEO */}
+
+      {/* JSON-LD pour SEO local et Google */}
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
@@ -53,8 +57,16 @@ export default function Head() {
             location: {
               "@type": "Place",
               name: "Stade Municipal d'Honfleur",
-              address: "14600 Honfleur, France",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "Stade Municipal d'Honfleur",
+                addressLocality: "Honfleur",
+                postalCode: "14600",
+                addressCountry: "FR",
+              },
             },
+            telephone: "06303230 76",
+            email: "schuster.axel@neuf.fr",
           }),
         }}
       />
